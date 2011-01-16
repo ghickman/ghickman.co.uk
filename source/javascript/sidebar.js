6773c7bb1a07d4830214ff713fbb2f9352d0f69b
George Hickman <george@ghickman.co.uk>
2011-01-16 09:50:19 +0000

    Added blog roll side bar

--- source/javascript/sidebar.js
+++ source/javascript/sidebar.js
@@ -1,30 +1,23 @@
-$(function() {
-    /**
-     * Slide takes the selector for one element in the side bar
-     * For example .categories
-     */
-    function slide(selector) {
-        $(selector + '.sidebar_title').click(function() {
-            if($(selector + '.sidebar_title').hasClass('hidden')) {
-                $(selector + '.sidebar_content').slideDown()
-                $(selector + '.sidebar_title')
-                    .removeClass('hidden')
-                    .css('border-bottom', 'none')
-                    .css('border-bottom-left-radius', '0px')
-                    .css('border-bottom-right-radius', '0px')
-            } else {
-                $(selector + '.sidebar_content').slideUp('slow', function() {
-                    $(selector + '.sidebar_title')
-                        .addClass('hidden')
-                        .css('border-bottom', '1px solid black')
-                        .css('border-bottom-left-radius', '7px')
-                        .css('border-bottom-right-radius', '7px')
-                    })
-            }
+jQuery(document).ready(function($) {
+  /**
+   * Slide takes the selector for one element in the side bar
+   * For example .categories
+   */
+  function slide(selector) {
+    $(selector + '.sidebar_title').click(function() {
+      if($(selector + '.sidebar_title').hasClass('hidden')) {
+        $(selector + '.sidebar_content').slideDown();
+        $(selector + '.sidebar_title').removeClass('hidden');
+      } else {
+        $(selector + '.sidebar_content').slideUp('slow', function() {
+          $(selector + '.sidebar_title').addClass('hidden');
         });
-    }
-    
-    slide('.categories')
-    slide('.archives')
-    slide('.twitter')
+      }
+    });
+  }
+
+  slide('.categories');
+  slide('.archives');
+  slide('.blogroll');
+  slide('.twitter');
 });
