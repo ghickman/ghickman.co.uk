c961deeddd03c7a5298e0d74e037c199c657e2d7
George Hickman <george@ghickman.co.uk>
2016-12-24 10:03:31 +0000

    Enshrine wasgijs in config and relayout

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from collections import OrderedDict
+
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
@@ -38,3 +40,39 @@ TEMPLATE_PAGES = {
 THEME = 'theme'
 
 TIMEZONE = 'Europe/London'
+
+WASGIJS = OrderedDict([(
+    'Original', [{
+        'image': 'http://www.wasgij.co.uk/images/uploads/puzzles/Original_20_Collections_Main.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/original/fishy-buisness',
+        'name': 'Fishy Business',
+        'number': '20',
+    }, {
+        'image': 'http://www.wasgij.com/images/uploads/puzzles/9_Home_Improvements.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/original/home-improvements',
+        'name': 'Home Improvements',
+        'number': '09',
+    }, {
+        'image': 'http://www.wasgij.com/images/uploads/puzzles/8_High_Tide.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/original/original-8',
+        'name': 'High Tide',
+        'number': '08',
+    }, {
+        'image': 'http://www.wasgij.com/images/uploads/puzzles/7_Bear_Necessities.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/original/original-7',
+        'name': 'Bear Necessities',
+        'number': '07',
+    }]), (
+    'Mystery', [{
+        'image': 'http://www.wasgij.com/images/uploads/puzzles/1_Wasgij_Express.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/mystery/mystery-1',
+        'name': 'Wasgij Express',
+        'number': '01'
+    }]), (
+    'Destiny', [{
+        'image': 'http://www.wasgij.com/images/uploads/puzzles/12_Market_Mayhem.jpg',
+        'link': 'http://www.wasgij.co.uk/collections/destiny/market-mayhem',
+        'name': 'Market Mayhem',
+        'number': '12',
+    }])
+])
